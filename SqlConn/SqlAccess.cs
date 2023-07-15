377f6e5d7c9a18686395fe5ceb6a291f89a2f9fb
Alexander Cheng <alexnader.engineering@gmail.com>
2023-07-15 19:05:11 +0800

    20230715 : SQL Connection 建立與Sql資料庫連結

---
+++ SqlConn/SqlAccess.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlConn
+{
+    public class SqlAccess
+    {
+        public static DataTable GetDataTable(SqlConnection sc, string sqlstr)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (SqlConnection sqlConn = new SqlConnection(sc.ConnectionString))
+                {
+                    sqlConn.Open();
+                    SqlCommand sqlComm = new SqlCommand();
+                    sqlComm.Connection = sqlConn;
+                    sqlComm.CommandTimeout = 3000;
+                    sqlComm.CommandText = sqlstr;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlComm);
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (SqlException e)
+            {
+                return null;
+            }
+        }
+        public static DataTable GetDataTable(SqlConnection sc, string sqlstr, int Timeout)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (SqlConnection sqlConn = new SqlConnection(sc.ConnectionString))
+                {
+                    sqlConn.Open();
+                    SqlCommand sqlComm = new SqlCommand();
+                    sqlComm.Connection = sqlConn;
+                    sqlComm.CommandTimeout = Timeout;
+                    sqlComm.CommandText = sqlstr;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlComm);
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (SqlException e)
+            {
+                return null;
+            }
+        }
+    }
+}
